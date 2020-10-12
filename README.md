e6112612040ff3241387d8557945acaa95e4f639
Indra <indra@imceria.com>
2020-10-12 15:34:45 +0700

    Init Base

---
+++ README.md
@@ -0,0 +1,1 @@
+# Base QUICK Template PWA
---
+++ src/App.svelte
@@ -0,0 +1,71 @@
+<style>
+	@import "//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+</style>
+<nav class="navbar">
+	<ul class="navbar-nav">
+		<li class="nav-logo nav-item">
+			<a href="#" class="nav-link">
+				<span class="link-text">Quickly</span>
+				<i class="link-icon fa fa-fw fa-angle-double-right"></i>
+			</a>
+		</li>
+		<li class="nav-item active">
+			<a href="#" class="nav-link">
+				<i class="link-icon fa fa-fw fa-star"></i>
+				<span class="link-text">Dashboard</span>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a href="#" class="nav-link">
+				<i class="link-icon fa fa-fw fa-bell"></i>
+				<span class="link-text">Notification</span>
+				<span class="link-badge">10</span>
+			</a>
+		</li>
+
+
+	</ul>
+</nav>
+<main>
+	<section class="v-row-center">
+
+	</section>
+	<section class="section-auto bg-secondary v-row-center h-row-center">
+		<div class="section-body" style="text-align: center;">
+			<h3 class="mb-big">Why So Quickly?</h3>
+			<ul class="box">
+				<li class="do-rotation">
+					<i class="rotate-this fa fa-feather-alt fa-3x"></i>
+					<h4 class="cl-theme">Lightweight</h4>
+					<p>Dibuat menggunakan CSS murni tanpa framework.</p>
+				</li>
+				<li class="do-rotation">
+					<i class="rotate-this fab fa-js fa-3x"></i>
+					<h4 class="cl-theme">No Javascript</h4>
+					<p>Tak perlu konfigurasi javascript lebih lanjut</p>
+				</li>
+				<li class="do-rotation">
+					<i class="rotate-this fa fa-mobile fa-3x"></i>
+					<h4 class="cl-theme">Responsive</h4>
+					<p>Ukuran sudah menyesuaikan dengan berbagai perangkat.</p>
+				</li>
+				<li class="do-rotation">
+					<i class="rotate-this fa fa-paint-roller fa-3x"></i>
+					<h4 class="cl-theme">Custom Themes</h4>
+					<p>Memiliki tema yang dapat diganti dengan mudah.</p>
+				</li>
+			</ul>
+		</div>
+	</section>
+	<section class="section-auto">
+		<div class="section-body">
+			<h3 style="margin-bottom: 1rem;">Future Plan</h3>
+			<p>Quickly masih dalam tahap pengembangan awal. Ada banyak sekali fitur yang ingin saya tambahkan di kemudian hari meliputi penulisan dokumentasi yang lengkap, kelas utility penunjang, kelas warna yang menyesuaikan tema, dukungan berbagai css framework, javscript API, dan berbagai tema baru lainnya, serta penyesuaian struktur agar template ini cocok digunakan sebagai dashboard maupun landing page.</p>
+			<h3 style="margin-bottom: 1rem;">Lisensi</h3>
+			<p>Sorce Code ini berada dalam naungan lisensi <a href="https://choosealicense.com/licenses/mit/">MIT</a> yang artinya anda bisa menggunakan/memodifikasi/mendistribusikan source code ini untuk kebutuhan pribadi maupun komersil dengan ketentuan lisensi ini tetap diikutsertakan.</p>
+			<h3 style="margin-bottom: 1rem;">Kolaborasi</h3>
+			<p>Saya sangat mengapresiasi apabila anda ingin berkolaborasi mengembangkan template ini lebih lanjut. Anda bisa melihat roadmap maupun list fitur yang sedang atau akan dikerjakan pada bagian <a href="https://github.com/itsyaqin/quickly/projects">Projects</a>.</p>
+		</div>
+	</section>
+
+</main>
